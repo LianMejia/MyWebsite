339aed0a0d66ca51889f2e0b347f242406ae5bfb
Lian Mejia <lianmejiag@gmail.com>
2020-12-25 18:21:35 -0500

    Se agrego la libreria react-router-dom para realizar una SPA y se crearon las carpetas pertenecientes al Header

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Layout from './../Layout';
+function App() {
+    return(
+        <BrowserRouter>
+           <Layout>
+               <Switch>
+
+               </Switch>
+           </Layout>
+        </BrowserRouter>
+    );
+}
+
+export default App;
