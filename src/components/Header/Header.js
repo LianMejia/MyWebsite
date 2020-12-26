44225705a9d520a6a3a64409936c989da03b142a
Lian Mejia <lianmejiag@gmail.com>
2020-12-25 19:51:34 -0500

    Creacion del header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 class Header extends React.Component{
     render(){
         return(
+            <section>
                 <div className="Titulo">
                     <p>MiWeb</p>
-                </div> 
+                </div>
+                <nav>
+                    <ul>
+                        <li><Link to="/Home">Home</Link></li>
+                        <li><Link to="/">About</Link></li>
+                        <li><Link to ="/">Services</Link></li>
+                        <li><Link to ="/">Contact</Link></li>
+                    </ul>
+                </nav>
+            </section>
         );
     }
 }
